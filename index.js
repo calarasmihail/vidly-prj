19c64c5d586d86b23a41a377af40a86d03c21975
Mihail Calaras <mihail.calaras@reigncode.dev>
2021-03-09 00:04:35 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Joi = require('joi');
+const genres = require('./routes/genres');
+const home = require('./routes/home');
+const app = express();
+
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/', home);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ routes/genres.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+const genres = [
+    {id: 1, name: 'Action'},
+    {id: 2, name: 'Adventure'},
+    {id: 3, name: 'Fantasy'}
+];
+
+router.get('/', (req, res) => {
+    res.send(genres);
+});
+
+router.post('/', (req, res) => {
+    const { error } = validateGenre(req.body); //equivalent to result.error
+    if (error) return res.status(404).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    genres.push(genre);
+    res.send(genre);
+});
+
+router.put('/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send('The genre with the given id is not found');
+    
+    // Object destructuring
+    const { error } = validateGenre(req.body); //equivalent to result.error
+    if (error) return res.status(404).send(error.details[0].message);
+
+    genre.name = req.body.name;
+    res.send(genre);
+});
+
+router.delete('/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send('The genre with the given id is not found');
+
+    const index = genres.indexOf(genre);
+    genre.splice(index, 1);
+
+    res.send(genre); 
+});
+
+router.get('/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send('The genre with the given id is not found');
+    res.send(genre);
+});
+
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(genre, schema);
+}
+
+module.exports = router;
+
